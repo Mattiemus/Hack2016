7372c1bb7bf6733f19dd9947b1d3b8cbb08729bc
Matthew Wade <mattie9875@hotmail.co.uk>
2016-03-19 18:37:58 +0000

    Started generalising the actions

--- routes/action.ts
+++ routes/action.ts
@@ -5,63 +5,123 @@ var mongodb = require('mongodb');
 import express = require('express');
 var router = express.Router();
 
-/* GET get. */
-router.get('/get/location', function(req, res, next) {
-    var cursor = req.db.collection('locations').find();
-    var result = [];
-    cursor.each((err, doc) => {
-        if (err != null) {
+function createRouter(collectionName) {
+    /* POST - Create */
+    router.post('/insert/' + collectionName, function(req, res, next) {
+        if(!req.body.name || req.body.name.length == 0) {
             res.send({
                 error: true,
-                reason: err
+                reason: 'An name must be specified'
             });
-        } else {
-            if (doc != null) {
-                result.push(doc);
-            } else {
-                res.send({
-                    error: false,
-                    result: result
-                });
-            }
         }
-      });
-});
+        if(!req.body.longitude || req.body.longitude.length == 0) {
+            res.send({
+                error: true,
+                reason: 'An longitude must be specified'
+            });
+        }
+        if(!req.body.latitude || req.body.latitude.length == 0) {
+            res.send({
+                error: true,
+                reason: 'An latitude must be specified'
+            });
+        }
 
-/* POST insert. */
-router.post('/insert/location', function(req, res, next) {
-    if(!req.body.name || req.body.name.length == 0) {
-        throw new Error('A name must be specified');
-    }
-    if(!req.body.longitude || req.body.longitude.length == 0) {
-        throw new Error('A longitude must be specified');
-    }
-    if(!req.body.latitude || req.body.latitude.length == 0) {
-        throw new Error('A latitude must be specified');
-    }
-
-    req.db.collection('locations').insertOne(req.body, (err, result) => {
-        res.send({
-            error: (err != null),
-            reason: (err != null) ? err : undefined,
-            result: (err == null) ? result : undefined
+        req.db.collection(collectionName).insertOne(req.body, (err, result) => {
+            res.send({
+                error: (err != null),
+                reason: (err != null) ? err : undefined,
+                result: (err == null) ? result : undefined
+            });
         });
     });
-});
 
-/* POST delete. */
-router.post('/delete/location', function(req, res, next) {
-    if(!req.body.id || req.body.id.length == 0) {
-        throw new Error('A id must be specified');
-    }
+    /* GET - Read */
+    router.get('/get/' + collectionName, function(req, res, next) {
+        var cursor = req.db.collection(collectionName).find();
+        var result = [];
+        cursor.each((err, doc) => {
+            if (err != null) {
+                res.send({
+                    error: true,
+                    reason: err
+                });
+            } else {
+                if (doc != null) {
+                    result.push(doc);
+                } else {
+                    res.send({
+                        error: false,
+                        result: result
+                    });
+                }
+            }
+          });
+    });
 
-    req.db.collection('locations').deleteOne({ "_id": new mongodb.ObjectID(req.body.id) }, (err, result) => {
-        res.send({
-            error: (err != null),
-            reason: (err != null) ? err : undefined,
-            result: (err == null) ? result : undefined
+    /* POST - Update */
+    router.post('/update/' + collectionName, function(req, res, next) {
+        if(!req.body.id || req.body.id.length == 0) {
+            res.send({
+                error: true,
+                reason: 'An id must be specified'
+            });
+        }
+
+        var cursor = req.db.collection(collectionName).find({ "_id": new mongodb.ObjectID(req.body.id) });
+        var result = [];
+        cursor.each((err, doc) => {
+            if (err != null) {
+                res.send({
+                    error: true,
+                    reason: err
+                });
+            } else {
+                if (doc != null) {
+                    if(!req.body.name || req.body.name.length == 0) {
+                        req.body.name = doc.name;
+                    }
+                    if(!req.body.longitude || req.body.longitude.length == 0) {
+                        req.body.longitude = doc.longitude;
+                    }
+                    if(!req.body.latitude || req.body.latitude.length == 0) {
+                        req.body.latitude = doc.latitude;
+                    }
+
+                    req.db.collection(collectionName).updateOne({ "_id": new mongodb.ObjectID(req.body.id) },
+                        req.body,
+                        null,
+                        (err, result) => {
+                            res.send({
+                                error: (err != null),
+                                reason: (err != null) ? err : undefined,
+                                result: (err == null) ? result : undefined
+                            });
+                        });
+                }
+            }
+          });
+    });
+
+    /* POST - Delete */
+    router.post('/delete/' + collectionName, function(req, res, next) {
+        if(!req.body.id || req.body.id.length == 0) {
+            res.send({
+                error: true,
+                reason: 'An id must be specified'
+            });
+        }
+
+        req.db.collection(collectionName).deleteOne({ "_id": new mongodb.ObjectID(req.body.id) }, (err, result) => {
+            res.send({
+                error: (err != null),
+                reason: (err != null) ? err : undefined,
+                result: (err == null) ? result : undefined
+            });
         });
     });
-});
+}
+
+createRouter('locations');
 
 export = router;
