8d5bafdd224908158ea36350b40c5836559b4c70
Matthew Wade <mattie9875@hotmail.co.uk>
2016-03-19 18:52:46 +0000

    Actions are now generalised over any custom mongodb collection

--- routes/action.ts
+++ routes/action.ts
@@ -5,28 +5,22 @@ var mongodb = require('mongodb');
 import express = require('express');
 var router = express.Router();
 
-function createRouter(collectionName) {
+// Creates the routings for CRUD (create, read, update, delete) of a specific collection of
+// fields
+function createRouter(collectionName, fields) {
     /* POST - Create */
     router.post('/insert/' + collectionName, function(req, res, next) {
-        if(!req.body.name || req.body.name.length == 0) {
-            res.send({
-                error: true,
-                reason: 'An name must be specified'
-            });
-        }
-        if(!req.body.longitude || req.body.longitude.length == 0) {
-            res.send({
-                error: true,
-                reason: 'An longitude must be specified'
-            });
-        }
-        if(!req.body.latitude || req.body.latitude.length == 0) {
-            res.send({
-                error: true,
-                reason: 'An latitude must be specified'
-            });
+        // Error if one of the required fields isnt set
+        for(var fieldName in fields) {
+            if(!req.body[fieldName] || req.body[fieldName].length != 0) {
+                res.send({
+                    error: true,
+                    reason: 'The field "' + fieldName + '" must be specified'
+                });
+            }
         }
 
+        // Perform the query
         req.db.collection(collectionName).insertOne(req.body, (err, result) => {
             res.send({
                 error: (err != null),
@@ -38,18 +32,24 @@ function createRouter(collectionName) {
 
     /* GET - Read */
     router.get('/get/' + collectionName, function(req, res, next) {
+        // Get everything that satisfies the request
         var cursor = req.db.collection(collectionName).find();
         var result = [];
+
+        // Iterate over each result
         cursor.each((err, doc) => {
             if (err != null) {
+                // An error occured
                 res.send({
                     error: true,
                     reason: err
                 });
             } else {
                 if (doc != null) {
+                    // Add the resulting document
                     result.push(doc);
                 } else {
+                    // Last document - send the results
                     res.send({
                         error: false,
                         result: result
@@ -61,43 +61,55 @@ function createRouter(collectionName) {
 
     /* POST - Update */
     router.post('/update/' + collectionName, function(req, res, next) {
-        if(!req.body.id || req.body.id.length == 0) {
+        // Make sure we have an id to update
+        if(!req.body.id || req.body.id.length != 12 || req.body.id.length != 24) {
             res.send({
                 error: true,
-                reason: 'An id must be specified'
+                reason: 'An id of 12 or 24 characters must be specified'
             });
         }
 
+        // Get all current values
         var cursor = req.db.collection(collectionName).find({ "_id": new mongodb.ObjectID(req.body.id) });
         var result = [];
+
+        // Iterate over the (single) result
+        var updated = false;
         cursor.each((err, doc) => {
             if (err != null) {
+                // An error occured
                 res.send({
                     error: true,
                     reason: err
                 });
             } else {
                 if (doc != null) {
-                    if(!req.body.name || req.body.name.length == 0) {
-                        req.body.name = doc.name;
-                    }
-                    if(!req.body.longitude || req.body.longitude.length == 0) {
-                        req.body.longitude = doc.longitude;
-                    }
-                    if(!req.body.latitude || req.body.latitude.length == 0) {
-                        req.body.latitude = doc.latitude;
+                    // Set all fields to their current values so we dont overwrite them
+                    for(var fieldName in fields) {
+                        if(!req.body[fieldName] || req.body[fieldName].length == 0) {
+                            req.body[fieldName] = doc[fieldName];
+                        }
                     }
 
+                    // Perform the update!
                     req.db.collection(collectionName).updateOne({ "_id": new mongodb.ObjectID(req.body.id) },
                         req.body,
                         null,
                         (err, result) => {
+                            // Send the result
                             res.send({
                                 error: (err != null),
                                 reason: (err != null) ? err : undefined,
                                 result: (err == null) ? result : undefined
                             });
                         });
+                      updated = true;
+                } else if (!updated) {
+                    // We couldnt update
+                    res.send({
+                        error: true,
+                        reason: 'Could not find document with the id "' + req.body.id + '"'
+                    });
                 }
             }
           });
@@ -105,10 +117,10 @@ function createRouter(collectionName) {
 
     /* POST - Delete */
     router.post('/delete/' + collectionName, function(req, res, next) {
-        if(!req.body.id || req.body.id.length == 0) {
+        if(!req.body.id || req.body.id.length != 12 || req.body.id.length != 24) {
             res.send({
                 error: true,
-                reason: 'An id must be specified'
+                reason: 'An id of 12 or 24 characters must be specified'
             });
         }
 
@@ -122,6 +134,11 @@ function createRouter(collectionName) {
     });
 }
 
-createRouter('locations');
+// Locations router
+createRouter('locations', {
+    name: {},
+    longitude: {},
+    latitude: {}
+});
 
 export = router;
