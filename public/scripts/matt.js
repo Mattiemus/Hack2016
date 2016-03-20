244ed278f0b5b882795830a477087bd80fbc4f2d
Matthew Wade <mattie9875@hotmail.co.uk>
2016-03-20 04:59:32 +0000

    More skill bubbles

--- public/scripts/matt.js
+++ public/scripts/matt.js
@@ -1,43 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("/action/get/people?location.$id=56edea6763115f9c0ebd0141", function(data) {
-        var events = data.result.map(function(value) {
-            return {
-                id: value._id,
-                title: value.firstname + " " + value.lastname + "'s Review",
-                start: value.reviewDate
-            };
-        });
-
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: true,
-            eventLimit: true,
-            events: events,
-            eventClick: function(event, jsEvent, view) {
-                console.log(event.id);
-            }
-        });
-    });
-
-    $.getJSON("/action/get/people", function(data) {
-        var skills = [];
-        data.result.forEach(function(value) {
-            skills.push(value.skills);
-        });
-        skills = [].concat.apply([], skills);
-
-        for (var i = 0, j = skills.length; i < j; i++) {
-            for(var j = 0; j < skills.length; j++) {
-                if(skills[j].name == skills[i].name) {
-                    skills[j].size = (skills[j].size || 0) + 1;
-                }
-            }
-        }
-
+    function displayBubbles(skills) {
         var diameter = 400,
             format = d3.format(",d"),
             color = d3.scale.category20c();
@@ -47,7 +9,8 @@ $(document).ready(function() {
             .size([diameter, diameter])
             .padding(1.5);
 
-        var svg = d3.select("body").append("svg")
+        var svg = d3.select("body")
+            .append("svg")
             .attr("width", diameter)
             .attr("height", diameter)
             .attr("class", "bubble");
@@ -95,5 +58,23 @@ $(document).ready(function() {
         }
 
         d3.select(self.frameElement).style("height", diameter + "px");
+    }
+
+    $.getJSON("/action/get/people", function(data) {
+        var skills = [];
+        data.result.forEach(function(value) {
+            skills.push(value.skills);
+        });
+        skills = [].concat.apply([], skills);
+
+        for (var i = 0, j = skills.length; i < j; i++) {
+            for(var j = 0; j < skills.length; j++) {
+                if(skills[j].name == skills[i].name) {
+                    skills[j].size = (skills[j].size || 0) + 1;
+                }
+            }
+        }
+
+        displayBubbles(skills);
     });
 });
