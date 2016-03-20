a4330c010a627741222b8371375f115e0205bc3a
Matthew Wade <mattie9875@hotmail.co.uk>
2016-03-20 10:12:15 +0000

    Finished skills search

--- public/scripts/matt.js
+++ public/scripts/matt.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
+    function filterSkills(skillName, categoryName, callback) {
+        var query = "";
+        query += skillName ? "name=" + skillName : "";
+        query += categoryName ? (query.length == 0 ? "" : "&") + "category=" + categoryName : "";
+        $.getJSON("/action/get/skills" + ((query.length != 0) ? "?" + query : ""), function(data) {
+            var skills = [];
+            for(var i = 0; i < data.result.length; i++) {
+                var found = false;
+                for(var j = 0; j < skills.length; j++) {
+                    if(skills[j].name == data.result[i].name) {
+                        found = true;
+                    }
+                }
+
+                if(!found) {
+                    skills.push(data.result[i]);
+                }
+            }
+
+            callback(skills);
+        });
+    }
+
+    filterSkills("Programming", 'Technical', function(data) {
+        console.log(data);
+    });
+
     function queryPeople(query, callback) {
         $.getJSON("/action/get/people", function(data) {
             data.result = data.result.filter(function (person) {
@@ -33,8 +60,8 @@ $(document).ready(function() {
         });
     }
 
-    queryPeople({ location: 'Londin' }, function(data) {
-        console.log(data.result);
-    });
+    //queryPeople({ skills: 'Prog' }, function(data) {
+    //    console.log(data.result);
+    //});
 
 });
